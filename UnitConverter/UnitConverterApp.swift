8df11101754680854fd4002407125b35904ac8cb
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-09-30 03:58:25 +0200

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Zaid Neurothrone on 2022-09-30.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
